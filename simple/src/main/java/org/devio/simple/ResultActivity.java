a9e422b042cbe644f8228ef8bb2459b2ae6f7325
timeyoyo <timeyoyo@gmail.com>
2018-08-10 17:17:32 +0800

    #1 提交疏通

--- simple/src/main/java/org/devio/simple/ResultActivity.java
+++ simple/src/main/java/org/devio/simple/ResultActivity.java
@@ -6,8 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import com.squareup.picasso.Picasso;
 
-import com.bumptech.glide.Glide;
+//import com.bumptech.glide.Glide;
 
 import org.devio.takephoto.model.TImage;
 
@@ -52,14 +53,14 @@ public class ResultActivity extends Activity {
             View view = LayoutInflater.from(this).inflate(R.layout.image_show, null);
             ImageView imageView1 = (ImageView) view.findViewById(R.id.imgShow1);
             ImageView imageView2 = (ImageView) view.findViewById(R.id.imgShow2);
-            Glide.with(this).load(new File(images.get(i).getCompressPath())).into(imageView1);
-            Glide.with(this).load(new File(images.get(i + 1).getCompressPath())).into(imageView2);
+            Picasso.with(this).load(new File(images.get(i).getCompressPath())).into(imageView1);
+            Picasso.with(this).load(new File(images.get(i + 1).getCompressPath())).into(imageView2);
             linearLayout.addView(view);
         }
         if (images.size() % 2 == 1) {
             View view = LayoutInflater.from(this).inflate(R.layout.image_show, null);
             ImageView imageView1 = (ImageView) view.findViewById(R.id.imgShow1);
-            Glide.with(this).load(new File(images.get(images.size() - 1).getCompressPath())).into(imageView1);
+            Picasso.with(this).load(new File(images.get(images.size() - 1).getCompressPath())).into(imageView1);
             linearLayout.addView(view);
         }
 
